92331017c6d7aee6734a98edd7dbb20f6e2746ec
Vincent Berenz <vincentberenz@users.noreply.github.com>
2020-04-01 17:45:35 +0200

    added a few get/set python wrappers (#18)
    Co-authored-by: Vincent Berenz <vberenz@tuebingen.mpg.de>

--- srcpy/wrappers.cpp
+++ srcpy/wrappers.cpp
@@ -11,53 +11,103 @@
 
 using namespace shared_memory;
 
-PYBIND11_MODULE(shared_memory_py, m)
-{
-    // Mutex
+PYBIND11_MODULE(shared_memory_py,m){
 
-    pybind11::class_<Mutex>(m, "Mutex")
-        .def(pybind11::init<std::string, bool>())
-        .def("unlock", &Mutex::unlock)
-        .def("lock", &Mutex::lock)
-        .def_static("clean", &Mutex::clean);
 
-    // Lock
+  // Mutex
+  
+  pybind11::class_<Mutex>(m,"Mutex")
+    .def(pybind11::init<std::string,bool>())
+    .def("unlock",&Mutex::unlock)
+    .def("lock",&Mutex::lock)
+    .def_static("clean",&Mutex::clean);
 
-    pybind11::class_<Lock>(m, "Lock").def(pybind11::init<Mutex&>());
+  // Lock
+  
+  pybind11::class_<Lock>(m,"Lock")
+    .def(pybind11::init<Mutex&>());
 
-    // ConditionVariable
+  // ConditionVariable
+  
+  pybind11::class_<ConditionVariable>(m,"ConditionVariable")
+    .def(pybind11::init<std::string,bool>())
+    .def("notify_all",&ConditionVariable::notify_all)
+    .def("notify_one",&ConditionVariable::notify_one)
+    .def("timed_wait",&ConditionVariable::timed_wait)
+    .def("wait",&ConditionVariable::wait)
+    .def_static("clean",&ConditionVariable::clean);
 
-    pybind11::class_<ConditionVariable>(m, "ConditionVariable")
-        .def(pybind11::init<std::string, bool>())
-        .def("notify_all", &ConditionVariable::notify_all)
-        .def("notify_one", &ConditionVariable::notify_one)
-        .def("timed_wait", &ConditionVariable::timed_wait)
-        .def("wait", &ConditionVariable::wait)
-        .def_static("clean", &ConditionVariable::clean);
+  // LockedConditionVariable
+  
+  pybind11::class_<LockedConditionVariable>(m,"LockedConditionVariable")
+    .def(pybind11::init<std::string,bool>())
+    .def("notify_all",&LockedConditionVariable::notify_all)
+    .def("notify_one",&LockedConditionVariable::notify_one)
+    .def("timed_wait",&LockedConditionVariable::timed_wait)
+    .def("wait",&LockedConditionVariable::wait)
+    .def("try_lock",&LockedConditionVariable::try_lock)
+    .def("unlock",&LockedConditionVariable::unlock)
+    .def("lock_scope",&LockedConditionVariable::lock_scope)
+    .def("unlock_scope",&LockedConditionVariable::unlock_scope)
+    .def_static("clean",&LockedConditionVariable::clean);
 
-    // LockedConditionVariable
 
-    pybind11::class_<LockedConditionVariable>(m, "LockedConditionVariable")
-        .def(pybind11::init<std::string, bool>())
-        .def("notify_all", &LockedConditionVariable::notify_all)
-        .def("notify_one", &LockedConditionVariable::notify_one)
-        .def("timed_wait", &LockedConditionVariable::timed_wait)
-        .def("wait", &LockedConditionVariable::wait)
-        .def("try_lock", &LockedConditionVariable::try_lock)
-        .def("unlock", &LockedConditionVariable::unlock)
-        .def("lock_scope", &LockedConditionVariable::lock_scope)
-        .def("unlock_scope", &LockedConditionVariable::unlock_scope)
-        .def_static("clean", &LockedConditionVariable::clean);
+  // Shared memory
 
-    // Shared memory
+  // sharing a boolean
+  m.def("set_bool",[](std::string segment,
+		      std::string object,
+		      bool value){
+	  shared_memory::set<bool>(segment,object,value);
+	});
+  m.def("get_bool",[](std::string segment,
+		      std::string object){
+	  bool value;
+	  shared_memory::get<bool>(segment,object,value);
+	  return value;
+	});
 
-    // sharing a boolean
-    m.def("set_bool", [](std::string segment, std::string object, bool value) {
-        shared_memory::set<bool>(segment, object, value);
+  m.def("set_int",[](std::string segment,
+		      std::string object,
+		      int value){
+	  shared_memory::set<int>(segment,object,value);
+	});
+  m.def("get_int",[](std::string segment,
+		      std::string object){
+	  int value;
+	  shared_memory::get<int>(segment,object,value);
+	  return value;
+	});
+
+  m.def("set_double",[](std::string segment,
+		      std::string object,
+		      double value){
+	  shared_memory::set<double>(segment,object,value);
+	});
+  m.def("get_double",[](std::string segment,
+		      std::string object){
+	  double value;
+	  shared_memory::get<double>(segment,object,value);
+	  return value;
+	});
+
+  m.def("set_string",[](std::string segment,
+		      std::string object,
+		      std::string value){
+	  shared_memory::set<std::string>(segment,object,value);
+	});
+  
+  m.def("get_string",[](std::string segment,
+		      std::string object){
+	  std::string value;
+	  shared_memory::get<std::string>(segment,object,value);
+	  return value;
+	});
+
+    
+  m.def("clear_shared_memory",[](std::string segment)
+    {
+      shared_memory::clear_shared_memory(segment);
     });
-    m.def("get_bool", [](std::string segment, std::string object) {
-        bool value;
-        shared_memory::get<bool>(segment, object, value);
-        return value;
-    });
+  
 }
